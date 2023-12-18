263e0bc37b4e69af52bd1c5c0888b4c07b4d23e7
uberfig <aidan.gifford04@gmail.com>
2023-12-18 01:08:39 -0500

    Initial commit

---
+++ src/book_intro.rs
@@ -0,0 +1,56 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
+}
+
+
+
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
+}
+
+
+
+
+fn hello_world() {
+    println!("hello world!");
+}
---
+++ src/main.rs
@@ -0,0 +1,158 @@
+use bevy::{
+    core_pipeline::clear_color::ClearColorConfig,
+    prelude::*,
+    render::{camera::ScalingMode, texture},
+};
+
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+}
+
+#[derive(Resource)]
+pub struct Money(pub f32);
+
+#[derive(Component)]
+pub struct PhysicsItem {
+    pub velocity: f32,
+    pub mass: f32,
+}
+
+#[derive(Component)]
+pub struct Newme {
+    pub lifetime: Timer,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest()) //nearest neighbor filtering by default for pixelart
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Ivy's secret project".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: true,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .insert_resource(Money(100.0))
+        .add_systems(Startup, setup) //run setup at the start of the program
+        .add_systems(Update, (character_movement, spawn_thing, newme_lifetime)) //run every frame
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // commands.spawn(
+    //     Camera2dBundle {
+    //         camera_2d: Camera2d { clear_color: ClearColorConfig::Custom(Color::GREEN), },
+    //         ..default() //takes the rest of the default props
+    //     }
+    // );
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 256.0,
+        min_height: 144.0,
+    };
+
+    commands.spawn(camera);
+
+    let texture = asset_server.load("textures/me.png");
+
+    //tuple of the sprite and a player
+    //when in a tuple, bevy treats it like a bundle
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(20.0, 20.0)),
+                ..default()
+            },
+            texture,
+            ..default()
+        },
+        Player { speed: 100.0 },
+    ));
+}
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Player)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, player) in &mut characters {
+        let movement_amount = player.speed * time.delta_seconds();
+
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += movement_amount;
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= movement_amount;
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += movement_amount;
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= movement_amount;
+        }
+    }
+}
+
+fn spawn_thing(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    input: Res<Input<KeyCode>>,
+    mut money: ResMut<Money>,
+    player: Query<&Transform, With<Player>>,
+) {
+    if !input.just_pressed(KeyCode::Space) {
+        return;
+    }
+    let player_transform = player.single(); //panics if theres more than one that matches the query, get single is recoverable
+
+    if money.0 >= 10.0 {
+        money.0 -= 10.0;
+        info!("Spent $10 to spawn, remaining money: ${:?}", money.0);
+    } else {
+        info!("not enough money, remaining money: ${:?}", money.0);
+        return;
+    }
+
+    let texture = asset_server.load("textures/newme.png");
+
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(20.0, 20.0)),
+                ..default()
+            },
+            texture,
+            transform: *player_transform,
+            ..default()
+        },
+        Newme {
+            lifetime: Timer::from_seconds(2.0, TimerMode::Once),
+        }
+    ));
+}
+
+fn newme_lifetime(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut newme: Query<(Entity, &mut Newme)>,
+    mut money: ResMut<Money>,
+) {
+    for (newme_entity, mut me) in &mut newme {
+        me.lifetime.tick(time.delta());
+
+        if me.lifetime.finished() {
+            money.0 += 15.0;
+
+            commands.entity(newme_entity).despawn();
+
+            info!("Newme dissapear for $15, Current money: ${:?}", money.0);
+        }
+    }
+}
